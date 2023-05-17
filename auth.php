0be1665e29e80aba5bb0ab805aa719dbf0fded00
Maro <maronomoto@gmail.com>
2023-05-17 22:45:18 +0900

    prototype

--- auth.php
+++ auth.php
@@ -14,21 +14,24 @@
         <li class="tab active">ログイン</li>
         <li class="tab">新規作成</li>
     </ul>
-    <div class="formbox">
-        <div class="panel active">
-        <form action="./login.php" method="post">
-            <input type="text" name="userID" id="userID" placeholder="userID"><br>
-            <input type="password" name="password" id="password" placeholder="password"><br>
-            <input type="submit" value="ログイン">
-        </form>
-        </div>
+        <div class="formbox">
+                <div class="panel active">
+                <label>ログイン</label>
+                <form action="./login.php" method="post">
+                    <!-- <label id="userID">ユーザーID</label><br> -->
+                    <input type="text" name="userID" id="userID" placeholder="userID" required="required"><br>
+                    <input type="password" name="password" id="password" placeholder="password" required="required"><br>
+                    <input type="submit" name="button" value="ログイン">
+                </form>
+            </div>
         <div class="panel">
+            <label>新規登録</label>
             <form action="./register.php" method="post">
-                <input type="text" name="userID" id="userID" placeholder="userID"><br>
-                <input type="text" name="userName" id="userName" placeholder="user name"><br>
-                <input type="email" name="userMail" id="userMail" placeholder="E-Mail address"><br>
-                <input type="password" name="pass" id="pass" placeholder="password"><br>
-                <input type="submit" value="新規登録" >
+                <input type="text" name="userID" id="userID" placeholder="userID" required="required"><br>
+                <input type="text" name="userName" id="userName" placeholder="user name" required="required"><br>
+                <input type="email" name="userMail" id="userMail" placeholder="E-Mail address" required="required"><br>
+                <input type="password" name="pass" id="pass" placeholder="password" required="required"><br>
+                <input type="submit" name="button" value="新規登録" >
             </form>
         </div>
     </div>
