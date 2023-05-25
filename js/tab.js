0d05762ceec9379c01e2804d9be2e96249a34397
Maro <maronomoto@gmail.com>
2023-05-25 10:22:14 +0900

    tab.js 追加

---
+++ js/tab.js
@@ -0,0 +1,15 @@
+//iconをクリックしたらusernavの表示/非表示
+let switchBtn = document.getElementsByTagName('button')[0];
+let nav = document.getElementById('userNav');
+let changeElement = (el)=> {
+  if(el.style.display=='none'){
+    el.style.display='block';
+  }else{
+    el.style.display='block';
+  }
+}
+
+
+switchBtn.addEventListener('click', ()=> {
+  changeElement(nav);
+}, false);
