f50547493416257fce176bfc40183773e61752e1
emiminate <matsu130830@gmail.com>
2023-06-19 14:09:24 +0900

    dounaru

--- JSONconnect.php
+++ JSONconnect.php
@@ -2,10 +2,37 @@
 include('./dbconnect.php');
 $userID = 1;
 $questID = 1;
-$questData = getquest($questID);
 $userData = getUser($userID);
+$questData = getquest($questID);
 
-echo $userData['userName'];
-echo $questData['questTitle'];
+$tileTitle = $userData['userName'];
 
+// ユーザーデータから既存のタイル情報を取得
+$existingTiles = $questData['questTile'];
+
+// 新しいタイルデータを作成
+$newTile = [
+  'tileTitle' => $tileTitle,
+  'tileContext' => 'なにやるか',
+  'tileColor' => 'red',
+  'x' => 40,
+  'y' => 30,
+  'nextTiles' => [1,2],
+  'backTiles' => [],
+  'tileCompleted' => false,
+  'tileExecutable' => true,
+  'questID' => []
+];
+
+// 既存のタイルデータに新しいタイルを追加
+$updatedTiles = $existingTiles;
+$updatedTiles[] = $newTile;
+
+// 更新されたタイルデータをユーザーデータに反映
+$userData['tiles'] = $updatedTiles;
+
+// JSON 形式にエンコード
+$jsonData = json_encode($userData);
+
+echo $jsonData;
 ?>
