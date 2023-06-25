ec8aad80ab898d0ba69557b5054feca88506be95
Maro <maronomoto@gmail.com>
2023-06-26 00:31:10 +0900

    fix usernav

--- js/tab.js
+++ js/tab.js
@@ -15,17 +15,22 @@ switchBtn.addEventListener('click', ()=> {
 
 
 
+function saveIcon(icon) {
+  // Get the src of the icon.
+  var iconSrc = icon.src;
 
-//アイコン色
-const colorOptions = document.querySelectorAll('.color-option');
-const userIcon = document.getElementById('userIcon');
+  // Set the src of the user's profile.
+  var userProfile = document.getElementById('user-profile');
+  userProfile.src = iconSrc;
+}
 
-colorOptions.forEach(option => {
-  option.addEventListener('click', function() {
-    const color = this.getAttribute('data-color');
-    userIcon.style.backgroundColor = color;
-  });
-});
+function changeIcon() {
+  // Get the src of the div element.
+  var divSrc = document.getElementById('userIcon').src;
 
+  // Set the src of the icon.
+  var icon = document.querySelector('ul li img:checked');
+  document.getElementById('userIcon').src = icon.src;
+}
 
 
