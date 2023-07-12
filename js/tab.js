cfc278bacfa8ac2951058686d9da2deb1d559fc0
Maro <maronomoto@gmail.com>
2023-07-12 14:28:47 +0900

    fix code

--- js/tab.js
+++ js/tab.js
@@ -1,4 +1,4 @@
-//iconをクリックしたらusernavの表示/非表示
+//iconをクリックしたらusernavの表示/非表示  functionかけ
 let switchBtn = document.getElementById('userIcon');
 let nav = document.getElementById('userNav');
 let changeElement = (el)=> {
@@ -9,13 +9,28 @@ let changeElement = (el)=> {
   }
 }
 
-switchBtn.addEventListener('click', ()=> {
+// switchBtn をクリックした場合の処理
+switchBtn.addEventListener('click', (event)=> {
+  // イベントの伝播を停止し、タブを開閉する
+  event.stopPropagation();
   changeElement(nav);
+});
+
+// ドキュメント全体に対してクリックイベントを追加
+document.addEventListener('click', (event) => {
+  // クリックされた要素が switchBtn または nav の子孫要素である場合は何もしない
+  if (event.target === switchBtn || nav.contains(event.target)) {
+    return;
+  }
+
+  // タブを閉じる
+  nav.style.display = 'none';
 });
 
 
 
 
+//アイコンの要素クリックでやるほうがよくね関数呼び出し
 //アイコン選択 選択したアイコンのsrcを変数に
 function saveIcon(element) {
   var src = element.getAttribute("src");
